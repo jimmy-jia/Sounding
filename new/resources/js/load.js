5a5d78929aff1cb5038823e1ad66dac8ecb17288
jimmy-jia <jimmy.j.jia@gmail.com>
2015-06-19 15:43:38 -0500

    put back jiggle

--- new/resources/js/load.js
+++ new/resources/js/load.js
@@ -44,7 +44,17 @@ function submitPost(){
 		}
 	}
 }
-
+jQuery.fn.shake = function(intShakes, intDistance, intDuration) {
+    this.each(function() {
+        $(this).css("position","relative"); 
+        for (var x=1; x<=intShakes; x++) {
+        $(this).animate({left:(intDistance*-1)}, (((intDuration/intShakes)/4)))
+    .animate({left:intDistance}, ((intDuration/intShakes)/2))
+    .animate({left:0}, (((intDuration/intShakes)/4)));
+    }
+  });
+return this;
+};
 // GET Items //
 var i=1;
 var j=16;
